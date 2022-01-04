bccf16ee69d968e15fb6b57a82fc59354305da9b
Anton Soroko <anton.soroko@gmail.com>
2022-01-04 23:15:59 +0300

    update opennicResolver (#78)
    * update opennicResolver
    
    * update opennicResolver IPs

--- proxy/dns.go
+++ proxy/dns.go
@@ -44,7 +44,7 @@ var (
 	}
 
 	commonResolver  = doh.Use(doh.CloudflareProvider, doh.GoogleProvider)
-	opennicResolver = dns_resolver.New([]string{"193.183.98.66", "172.104.136.243", "89.18.27.167"})
+	opennicResolver = dns_resolver.New([]string{"163.172.168.171", "152.70.189.130", "167.86.112.174"})
 
 	dnsCacheResults sync.Map
 	dnsCacheLocks   sync.Map
