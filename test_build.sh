58fcbe2e88ed3784debd6677f85d14d8694f3f5d
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2022-12-22 16:49:52 +0100

    Adjusted test_build.sh script to work in WSL environment.

--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 
+set -x 
 function test {
   echo "+ $@"
   "$@"
@@ -10,36 +11,54 @@ function test {
   return $status
 }
 
-GIT_VERSION=`cd ${GOPATH}/src/github.com/elgatito/elementum; git describe --tags`
+if [ -d "${GOPATH}/src/github.com/elgatito/elementum" ];
+then
+  GIT_VERSION=`cd ${GOPATH}/src/github.com/elgatito/elementum; git describe --tags`
+else
+  GIT_VERSION=`git describe --tags`
+fi
+
+DEST_MAKE=linux-x64
+DEST_PLATFORM=linux_x64
+DEST_DIR=$HOME/.kodi
+if [ ! -z "${WSL_USER}" ]; then
+  DEST_DIR=/mnt/c/Users/${WSL_USER}/AppData/Roaming/Kodi
+  DEST_PLATFORM=windows_x64
+  DEST_MAKE=windows-x64
+fi
 
 if [ "$1" == "local" ]
 then
+  LOCAL_ENV=$GOPATH/src/github.com/ElementumOrg/libtorrent-go/local-env/
+  if [ -d "$CROSS_ROOT" ];
+  then
+      LOCAL_ENV=$CROSS_ROOT
+  fi
+
   # This will run with local go using libtorrent-go/local-env/ locally copied dependencies compilation.
-  export LOCAL_ENV=$GOPATH/src/github.com/ElementumOrg/libtorrent-go/local-env/
+  export LOCAL_ENV=$LOCAL_ENV
   export PATH=$PATH:$LOCAL_ENV/bin/
   export PKG_CONFIG_PATH=$LOCAL_ENV/lib/pkgconfig
   export SWIG_LIB=$LOCAL_ENV/share/swig/4.1.0/
 
-  cd $GOPATH/src/github.com/elgatito/elementum
   set -e
   test go build -ldflags="-w -X github.com/elgatito/elementum/util.Version=${GIT_VERSION}" -o /var/tmp/elementum .
   test chmod +x /var/tmp/elementum
-  test cp -rf /var/tmp/elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/
-  test cp -rf /var/tmp/elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
+  test cp -rf /var/tmp/elementum* $DEST_DIR/addons/plugin.video.elementum/resources/bin/$DEST_PLATFORM/
+  test cp -rf /var/tmp/elementum* $DEST_DIR/userdata/addon_data/plugin.video.elementum/bin/$DEST_PLATFORM/
 elif [ "$1" == "sanitize" ]
 then
   # This will run with local go
-  cd $GOPATH
   set -e
   CGO_ENABLED=1 CGO_LDFLAGS='-fsanitize=leak -fsanitize=address' CGO_CFLAGS='-fsanitize=leak -fsanitize=address' test go build -ldflags="-w -X github.com/elgatito/elementum/util.Version=${GIT_VERSION}" -o /var/tmp/elementum github.com/elgatito/elementum
   test chmod +x /var/tmp/elementum
-  test cp -rf /var/tmp/elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/
-  test cp -rf /var/tmp/elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
+  test cp -rf /var/tmp/elementum* $DEST_DIR/addons/plugin.video.elementum/resources/bin/$DEST_PLATFORM/
+  test cp -rf /var/tmp/elementum* $DEST_DIR/userdata/addon_data/plugin.video.elementum/bin/$DEST_PLATFORM/
 elif [ "$1" == "docker" ]
 then
-  # This will run with docker libtorrent:linux-x64 image
-  cd $GOPATH/src/github.com/elgatito/elementum
-  test make linux-x64
-  test cp -rf build/linux_x64/elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/
-  test cp -rf build/linux_x64/elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
+  # This will run with docker libtorrent:$DEST_MAKE image
+  test make $DEST_MAKE
+  test cp -rf build/$DEST_PLATFORM/elementum* $DEST_DIR/addons/plugin.video.elementum/resources/bin/$DEST_PLATFORM/
+  # Windows is blocking this WSL path?
+  # test cp -rf build/$DEST_PLATFORM/elementum* $DEST_DIR/userdata/addon_data/plugin.video.elementum/bin/$DEST_PLATFORM/
 fi
